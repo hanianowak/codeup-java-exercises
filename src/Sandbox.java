365cf6bb7f3b93db9c9afc22dfdfee7e77a62a6f
hanianowak <hania.nowak2018@gmail.com>
2018-09-24 16:04:09 -0500

    sandbox

---
+++ src/Sandbox.java
@@ -0,0 +1,44 @@
+import sun.awt.geom.AreaOp;
+
+public class Sandbox {
+    public static void main(String[] args) {
+         countDown(8);
+
+        int[] numbers = {1,2,3,4,5,6,7,8,9};
+        for (int num:numbers) System.out.print(num + " ");
+        System.out.println();
+        for (int num:reverse(numbers)) System.out.print(num + " ");
+
+    }
+
+
+
+    public static int countDown(int input) {
+        if (input > 0) {
+            System.out.println(input);
+            return countDown(input - 1);
+        } else {
+            System.out.println("Blastoff");
+            return 0;
+        }
+    }
+
+
+    public static int[] reverse(int[] nums) {
+        int[] backwards = new int[nums.length];
+        for (int i =0;i<nums.length;i++) {
+            //backwards.unshift(nums[i])
+            for (int num:backwards)
+                System.out.print(num + " ");
+            System.out.println();
+            backwards[backwards.length - i - 1] = nums[i];
+        }
+        return backwards;
+    }
+
+
+
+}
+
+
+    
